28a8e8f4a3b9383de6729f71ab8ac4d583f36d02
andres <andres06energy@hotmail.com>
2022-02-03 18:02:20 -0300

    Updated DML

--- 03_db_microelectronica_DML_UPDATES.sql
+++ 03_db_microelectronica_DML_UPDATES.sql
@@ -70,8 +70,99 @@ where (componentes.nro_pieza = '3CD010G' and componentes.categoria = 'Transistor
 
 
 
+-- Reemplazamos el caracter 'a' por el  '-' del campo VOLTAJE_RECOMENDADO
+update (select voltaje_recomendado, voltaje_min_entrada, voltaje_max_entrada 
+, componentes.nro_pieza, componentes.categoria 
+from componentes_detalles inner join componentes 
+on componentes_detalles.id_componente = componentes.id
+where (componentes.nro_pieza = '3CD010G' and componentes.categoria = 'Transistores BJT')
+)set voltaje_recomendado = '3.5-5 VDC', voltaje_min_entrada = '3.5 VDC', voltaje_max_entrada = '12 VDC';
+
+
+
+
+update componentes_detalles set voltaje_recomendado = replace(voltaje_recomendado, 'a' , '-');
+
+
 
 select * from componentes;
 select * from componentes_detalles;
+
+
+
+
+-- ---------------------------------------------------------------------------
+-- ---------------------------------------------------------------------------
+
+
+-- ===========================================================
+-- ======= TABLA COMPONENTES_TRANSISTORES_BIPOLARES===========
+-- ===========================================================
+
+select * from componentes;
+select * from componentes_detalles;
+select * from componentes_transistores_bipolares;
+
+describe componentes_transistores_bipolares;
+
+
+-- Cambiamos el voltaje de colector emisor, voltaje colector base y voltaje emisor base segun el nro de pieza 
+-- y la descripcion determinado en la tabla componentes y el voltaje en componentes_detalles
+update (select voltaje_colec_emis, voltaje_colec_base , voltaje_emis_base
+, componentes.nro_pieza, componentes.descripcion
+, componentes_detalles.voltaje_recomendado
+from componentes_transistores_bipolares 
+inner join componentes on  componentes.id = componentes_transistores_bipolares.id_componente
+inner join componentes_detalles on componentes_detalles.id_componente = componentes_transistores_bipolares.id_componente 
+where (componentes.nro_pieza = '3CD010G' and componentes.descripcion = 'Transistor BJT PNP' 
+and componentes_detalles.voltaje_recomendado = '3.5-5 VDC')
+)set voltaje_colec_emis = '5.5 VDC', voltaje_colec_base = '5.5 VDC', voltaje_emis_base ='-5.5 VDC' ;
+
+
+-- Cambiamos la Temperatura de Juntura según el nro de pieza
+
+update (select temp_juntura
+, componentes.nro_pieza
+from componentes_transistores_bipolares
+inner join componentes on componentes.id = componentes_transistores_bipolares.id_componente
+where (componentes.nro_pieza = 'KSH13006')
+)set temp_juntura = '160°C';
+
+
+select * from componentes_transistores_bipolares;
+
+
+
+
+
+
+-- ---------------------------------------------------------------------------
+-- ---------------------------------------------------------------------------
+
+
+-- ===========================================================
+-- ======= TABLA COMPONENTES_TRANSISTORES_MOSFET===========
+-- ===========================================================
+
+select * from componentes;
+select * from componentes_detalles;
+select * from componentes_transistores_mosfet;
+
+describe componentes_transistores_mosfet;
+
+
+-- Nuleeamos campos sin descripcion
+update componentes_transistores_mosfet set conduct_drenaje_sustrato = null 
+where conduct_drenaje_sustrato = '-';
+
+update componentes_transistores_mosfet set resist_drenaje_fuente = null 
+where resist_drenaje_fuente = '-';
+
+
+-- Pasamos cadenas a Mayuscula
+update componentes_transistores_mosfet set tipo = upper(tipo);
+
+
+select * from componentes_transistores_mosfet;
 
 
